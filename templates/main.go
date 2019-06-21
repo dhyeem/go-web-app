d3205e3770fd0fdc25f7c4871739db667f91125a
dhyeem <dhyeem@live.com>
2019-06-21 18:36:21 +0300

    adding template fun and hanndlefuncs code fmt

--- templates/main.go
+++ templates/main.go
@@ -1,10 +1,47 @@
 package main
 
 import (
-	"fmt"
+	"html/template"
+	"log"
+	"net/http"
 )
 
+var tpl *template.Template
+
+func init() {
+	tpl = template.Must(template.ParseGlob("templates/*.gohtml"))
+}
+
 func main() {
-	//do somthinng
-	fmt.Print("hello")
+	http.HandleFunc("/index", idx)
+	http.HandleFunc("/about", abot)
+	http.HandleFunc("/contact", cntct)
+	http.HandleFunc("/apply", aply)
+	http.HandleFunc("", idx)
+	http.ListenAndServe(":8080", nil)
+}
+
+func idx(w http.ResponseWriter, req *http.Request) {
+	err := tpl.ExecuteTemplate(w, "index.gohtml", nil)
+	if err != nil {
+		log.Println(err)
+	}
+}
+func abot(w http.ResponseWriter, req *http.Request) {
+	err := tpl.ExecuteTemplate(w, "about.gohtml", nil)
+	if err != nil {
+		log.Println(err)
+	}
+}
+func cntct(w http.ResponseWriter, req *http.Request) {
+	err := tpl.ExecuteTemplate(w, "contact.gohtml", nil)
+	if err != nil {
+		log.Println(err)
+	}
+}
+func aply(w http.ResponseWriter, req *http.Request) {
+	err := tpl.ExecuteTemplate(w, "apply.gothml", nil)
+	if err != nil {
+		log.Println(err)
+	}
 }
