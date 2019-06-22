511a15b15e1c0fe5bd94d57d4ccc5e339a31e0eb
dhyeem <dhyeem@live.com>
2019-06-23 02:31:59 +0300

    adding routing adding page titles in templates play with templates
    end of session

--- templates/main.go
+++ templates/main.go
@@ -6,6 +6,11 @@ import (
 	"net/http"
 )
 
+type pageData struct {
+	Title     string
+	FirstName string
+}
+
 var tpl *template.Template
 
 func init() {
@@ -13,35 +18,63 @@ func init() {
 }
 
 func main() {
-	http.HandleFunc("/index", idx)
+	http.HandleFunc("/", idx)
 	http.HandleFunc("/about", abot)
 	http.HandleFunc("/contact", cntct)
 	http.HandleFunc("/apply", aply)
-	http.HandleFunc("", idx)
+	http.Handle("/index", http.NotFoundHandler())
 	http.ListenAndServe(":8080", nil)
 }
 
 func idx(w http.ResponseWriter, req *http.Request) {
-	err := tpl.ExecuteTemplate(w, "index.gohtml", nil)
-	if err != nil {
-		log.Println(err)
+	pd := pageData{
+		Title: "Index Page",
 	}
+	err := tpl.ExecuteTemplate(w, "index.gohtml", pd)
+	if err != nil {
+		log.Println("LOGG", err)
+		http.Error(w, "Internal Serverrrrr Error", http.StatusInternalServerError)
+		return
+
+	}
+
 }
 func abot(w http.ResponseWriter, req *http.Request) {
-	err := tpl.ExecuteTemplate(w, "about.gohtml", nil)
+	pd := pageData{
+		Title: "About Page",
+	}
+	err := tpl.ExecuteTemplate(w, "about.gohtml", pd)
 	if err != nil {
 		log.Println(err)
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+		return
 	}
 }
 func cntct(w http.ResponseWriter, req *http.Request) {
-	err := tpl.ExecuteTemplate(w, "contact.gohtml", nil)
+	pd := pageData{
+		Title: "Contact Page",
+	}
+	err := tpl.ExecuteTemplate(w, "contact.gohtml", pd)
 	if err != nil {
 		log.Println(err)
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+		return
 	}
 }
 func aply(w http.ResponseWriter, req *http.Request) {
-	err := tpl.ExecuteTemplate(w, "apply.gothml", nil)
+	pd := pageData{
+		Title: "Index Page",
+	}
+	var first string
+	if req.Method == http.MethodPost {
+		first = req.FormValue("fname")
+		pd.FirstName = first
+
+	}
+	err := tpl.ExecuteTemplate(w, "apply.gohtml", pd)
 	if err != nil {
 		log.Println(err)
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+		return
 	}
 }
